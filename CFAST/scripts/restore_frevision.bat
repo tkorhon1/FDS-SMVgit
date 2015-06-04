c2c626fb03228ef68ce42bf9c8c2130a7a5f7204
gforney <gforney@gmail.com>
2015-06-03 20:58:07 -0400

    add some CFAST .bat files to repo

---
+++ CFAST/scripts/restore_frevision.bat
@@ -0,0 +1,10 @@
+@echo off
+set file=%1
+
+if NOT exist %file% (
+  exit /b 1
+)
+
+call ..\scripts\contract_keyword Revision: %file%
+call ..\scripts\contract_keyword RevisionDate: %file%
+call ..\scripts\contract_keyword CompileDate: %file%
